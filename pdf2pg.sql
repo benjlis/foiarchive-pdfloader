baf1b1668cd7b4eb1c60c11c95c0c7a4264010cc
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-05-24 12:49:39 -0400

    Added range predicate to get-pdf-list query

--- pdf2pg.sql
+++ pdf2pg.sql
@@ -4,6 +4,7 @@ select row_number() over () , m.oai_id, pdf_url
     from un_archives.metadata_load m left join un_archives.pdfs p
                                         on (m.oai_id = p.oai_id)
     where pdf_url is not null and
+          m.oai_id between :first_id and :last_id and
           p.oai_id is null
     order by oai_id;
 
