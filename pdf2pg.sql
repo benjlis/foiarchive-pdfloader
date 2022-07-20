59fba3358c7c8e58f641888a8f1e1c126f61f38c
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-07-20 17:18:54 -0400

    Updated for NATO archives

--- pdf2pg.sql
+++ pdf2pg.sql
@@ -1,19 +1,22 @@
 -- name: get-pdf-list
 -- Get list of all pdfs that can be processed
-select row_number() over () , m.oai_id, pdf_url
-    from un_archives.metadata m left join un_archives.pdfs p
-                                        on (m.oai_id = p.oai_id)
-    where pdf_url is not null and
-          m.oai_id between :first_id and :last_id and
-          p.oai_id is null
-    order by oai_id;
-
+select row_number() over () , el.hlid, el.digitalobjecturi
+    from nato_archives.export_load el
+    where digitalobjecturi is not null and
+          identifier is not null and
+          hlid between :first_id and :last_id and
+          (culture = 'en' or
+           (culture = 'fr' and
+            not exists (select 1 from nato_archives.export_load
+                           where culture = 'en' and
+                                 identifier = el.identifier)))
+    order by hlid;
 -- name: add-pdf!
 -- Add row to pdfpages
-insert into un_archives.pdfs(oai_id, pg_cnt, size)
-values (:oai_id, :pg_cnt, :size);
+insert into nato_archives.pdfs(item_id, pg_cnt, size)
+values (:id, :pg_cnt, :size);
 
 -- name: add-pdfpage!
 -- Add row to pdfpages
-insert into un_archives.pdfpages(oai_id, pg, word_cnt, char_cnt, body)
-values (:oai_id, :pg, :word_cnt, :char_cnt, :body);
+insert into nato_archives.pdfpages(item_id, pg, word_cnt, char_cnt, body)
+values (:id, :pg, :word_cnt, :char_cnt, :body);
